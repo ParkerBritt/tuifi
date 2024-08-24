7694544011c79a1a6c33abbc89b63a62397064db
parker <parker.g.britt@gmail.com>
2024-08-24 21:52:15 +0100

    remove build dir

--- build/CMakeFiles/tui_app_launcher.dir/src/DesktopFile.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/tui_app_launcher.dir/src/DesktopFile.cpp.o: \
- /home/parker/MyRepos/tuifi/src/DesktopFile.cpp \
- /usr/include/stdc-predef.h \
- /home/parker/MyRepos/tuifi/include/DesktopFile.hpp
